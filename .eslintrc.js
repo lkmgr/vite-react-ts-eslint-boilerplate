e5d534fd790a7d61a8bd589a6e3b5221ab8e16d1
lkmgr <7467162+lkmgr@users.noreply.github.com>
2022-06-16 00:36:37 +0200

    🚀 initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'es5',
+};

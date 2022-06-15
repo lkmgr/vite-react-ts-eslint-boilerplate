0015c664800ef1d6e5c943fcf2a99024d0e8aef3
lkmgr <7467162+lkmgr@users.noreply.github.com>
2022-06-16 01:11:36 +0200

    fix: add react jsx-runtime eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
